9c6c83cf681342abe44a932926f65404de67d3f0
M-Elosegui <104083489+M-Elosegui@users.noreply.github.com>
2022-05-09 18:03:44 +0100

    final pomodoro added

--- Pomodoro Timer/main.py
+++ Pomodoro Timer/main.py
@@ -1,8 +1,7 @@
-
 from tkinter import *
+import math
 
-
-# ---------------------------- CONSTANTS ------------------------------- #
+# ---------------------------- CONSTANTS
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -11,48 +10,86 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- #
+# ---------------------------- TIMER RESET
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    global reps
+    reps = 0
+
+# ---------------------------- TIMER MECHANISM
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="LONG BREAK")
+
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="SHORT BREAK")
+    else:
+        count_down(work_sec)
+        title_label.config(text="WORK!!")
 
 
-
-# ---------------------------- TIMER MECHANISM ------------------------------- #
-
-
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+# ---------------------------- COUNTDOWN MECHANISM
 
 def count_down(count):
+
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    #we use canvas.config(what_item, what_value) to change elements inside the canva.
+    #if outside, we would use any_item.config(what_value)
     if count > 0:
-        window.after(1000, count_down, count-1)
-        print(count)
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
 
+# ---------------------------- UI SETUP - basic config
 
-# ---------------------------- UI SETUP ------------------------------- #
-#################  CRIAR UMA JANELA COM O VISUAL ESTATICO
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-title_label = Label(text="timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+title_label = Label(text="TIMER", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
 title_label.grid(column=1, row=0)
 
-canvas = Canvas(width=400, height=450, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(200,270, image=tomato_img)
-canvas.create_text(200,80,text="00:00", fill="white",font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100,112, image=tomato_img)
+timer_text = canvas.create_text(100,130,text="00:00", fill="white",font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1,row=1)
 
-###################  CRIAR OS BOTOES E FUNCOES UI
-start_button = Button(text="Start")
+
+# ---------------------------- BUTTONS
+
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command = reset_timer)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
-count_down(5)
 
 window.mainloop()
 
